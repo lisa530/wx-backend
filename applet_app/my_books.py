1a11a8c64c2320dbafabfe658e73caa57d019cd0
lisa <lisa_283022177@126.com>
2021-01-27 13:19:46 +0800

    书架：添加书籍到书架表中

--- applet_app/my_books.py
+++ applet_app/my_books.py
@@ -14,6 +14,7 @@ my_books_bp = Blueprint('mybook',__name__,url_prefix='/mybooks')
 @login_required
 @my_books_bp.route('/') # 使用蓝图绑定路由
 def mybooks_list():
+    """获取书架列表"""
     # 1.从g对象中获取用户id
     user_id = g.user_id
     # 2.查询书架中的所有书籍，按创建时间倒序排序
@@ -54,6 +55,35 @@ def mybooks_list():
                 'title': bk.book_name
             })
         return jsonify(data)
+
+
+@login_required
+@my_books_bp.route("/<book_id>",methods=['POST'])
+def add_book(book_id):
+    """添加书籍"""
+    # 1.添加登录验证装饰器
+    user_id = g.user_id
+    # 2.接收书籍id
+    # 3.根据书籍id查询书籍列表
+    # book = Book.query.filter(Book.book_id==book_id).first()
+    book = Book.query.filter_by(book_id=book_id).first()
+    # 书籍不存在提示错误信息
+    if not book:
+        return jsonify(msg='书籍不存在'),404
+    # 查询书架表中是否有该书籍数据
+    # book_shelf =BookShelf.query.filter(BookShelf.user_id==user_id,BookShelf.book_id==book_id).first()
+    book_shelf = BookShelf.query.filter_by(user_id=user_id,book_id=book_id).first()
+    # 如果书架没有书籍数据，添加书籍到书架中
+    if not book_shelf:
+        bk_shelf = BookShelf(user_id=user_id,book_id=book_id,book_name=book.book_name,cover=book.cover)
+        # 提交到数据库
+        db.session.add(bk_shelf)
+        db.session.commit()
+        # 返回添加成功的信息
+        return jsonify(msg='添加书籍到书架成功')
+
+    # 书籍中该书籍已存在，则提示信息
+    return jsonify(msg='书架中该书籍已在存在'),404
 
 
 
