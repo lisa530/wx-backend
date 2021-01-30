140cafec8b803e06a3a1c8ace95445161252207f
lisa <lisa_283022177@126.com>
2021-01-30 17:25:20 +0800

    配置：阅读器配置实现

--- applet_app/reader_config.py
+++ applet_app/reader_config.py
@@ -28,3 +28,28 @@ def preference():
     db.session.commit()
     # 5.返回结果
     return jsonify(msg='设置成功')
+
+
+@login_required
+@config_bp.route('/reader',methods=['POST'])
+def reader_config():
+    # 1.获取参数、亮度、字号、背景、翻页效果
+    brightness = request.json.get('brightness') # 亮度
+    fontsize = request.json.get('fontsize') # 文字大小
+    background = request.json.get('background') # B1 ~ B6 内置背景
+    turn = request.json.get('turn') # T1 仿真 T2 平滑 T3 无 翻页模式
+    # 2. 查询用户表,根据用户id查询用户信息
+    user = User.query.get(g.user_id)
+    # 3. 保存设置信息
+    if brightness:
+        user.brightness = brightness
+    if fontsize:
+        user.fontSize = fontsize
+    if background:
+        user.background = background
+    if turn:
+        user.turn = turn
+    db.session.add(user)
+    db.session.commit()
+    # 返回结果
+    return jsonify(msg='设置成功')
